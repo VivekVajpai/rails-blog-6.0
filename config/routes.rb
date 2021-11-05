583a9258dcfdefda9e14044491f2bc664ee214a6
Vivek Vajpai <vivekvajpai65@gmail.com>
2021-11-06 02:30:21 +0530

    Add Show and Delete links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to:'pages#about'
   # get 'articles' to:'application#articles'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
   # resource :articles, only: [] do
   #   member do
   #     get :show
