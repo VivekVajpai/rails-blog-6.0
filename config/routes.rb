f9cf765fba3bcbeae2c8def570ba2644f8aa0adf
Vivek Vajpai <vivekvajpai65@gmail.com>
2021-11-08 13:48:05 +0530

    Add signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to:'pages#about'
-  # get 'articles' to:'application#articles'
   resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   resources :articles
-  # resource :articles, only: [] do
-  #   member do
-  #     get :show
-  #   end
-  # end
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
